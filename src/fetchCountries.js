6eb1b57d28edec9dc4e2880586b107ecd72ffc23
Aleksey Knigin <alexyo2626@gmail.com>
2022-09-19 20:50:28 +0300

    fix base_url

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 import Notiflix from 'notiflix';
 
-BASE_URL = 'https://restcountries.com/v3.1/name/';
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 function fetchCountries(name) {
   return fetch(
